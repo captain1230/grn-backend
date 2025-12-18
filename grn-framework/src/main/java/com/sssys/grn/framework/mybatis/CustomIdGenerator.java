fee34b6c433cb61474c31653db80feb3cd6fd9e8
gaolikang <captain1230@163.com>
2025-12-18 08:49:23 +0800

    redis mybatis

--- grn-framework/src/main/java/com/sssys/grn/framework/mybatis/CustomIdGenerator.java
+++ grn-framework/src/main/java/com/sssys/grn/framework/mybatis/CustomIdGenerator.java
@@ -1,32 +1,34 @@
-//package com.sssys.grn.framework.mybatis;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.keyvalue.core.IdentifierGenerator;
-//import org.springframework.data.util.TypeInformation;
-//
-///**
-// * @Description:
-// * @Author: captain
-// * @Date: 2025/12/17
-// */
-//
-//public class CustomIdGenerator implements IdentifierGenerator {
-//    @Autowired
-//    private Snowflake snowflake;
-//
-//    @Override
-//    public Long nextId(Object entity) {
-//        long id = snowflake.nextId();
-//        //返回生成的id值即可.
-//        return id;
-//    }
-//
-//    public void setSnowflake(Snowflake snowflake) {
-//        this.snowflake = snowflake;
-//    }
-//
-//    @Override
-//    public <T> T generateIdentifierOfType(TypeInformation<T> type) {
-//        return null;
-//    }
-//}
+package com.sssys.grn.framework.mybatis;
+
+
+import cn.hutool.core.lang.Snowflake;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.TypeInformation;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description:
+ * @Author: captain
+ * @Date: 2025/12/17
+ */
+@Component
+public class CustomIdGenerator implements IdentifierGenerator {
+    @Autowired
+    private Snowflake snowflake;
+
+    @Override
+    public Long nextId(Object entity) {
+        long id = snowflake.nextId();
+        //返回生成的id值即可.
+        return id;
+    }
+
+    public void setSnowflake(Snowflake snowflake) {
+        this.snowflake = snowflake;
+    }
+
+    public <T> T generateIdentifierOfType(TypeInformation<T> type) {
+        return null;
+    }
+}
---
+++ grn-framework/src/main/java/com/sssys/grn/framework/security/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.sssys.grn.framework.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * @Description:
+ * @Author: captain
+ * @Date: 2025/12/18
+ */
+
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        // 实现JWT验证逻辑
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            // 去掉 "Bearer "
+            String token = authHeader.substring(7);
+            // 在这里添加你的JWT解析和认证逻辑
+        }
+        filterChain.doFilter(request, response);
+    }
+}

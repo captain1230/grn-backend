3148abbb37a96dce6d767ae56231276b025d9a08
captain <captain1230@163.com>
2025-12-18 16:45:53 +0800

    mysql数据库引入 代码初始化

--- grn-server/src/main/java/com/sssys/grn/GrnServerApplication.java
+++ grn-server/src/main/java/com/sssys/grn/GrnServerApplication.java
@@ -2,8 +2,10 @@ package com.sssys.grn;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,12 +14,18 @@ import org.springframework.context.annotation.ComponentScan;
  * @Author: captain
  * @Date: 11:12 2025/12/18
  */
-@SpringBootApplication(scanBasePackages = "com.sssys.grn",exclude = {
-        DataSourceAutoConfiguration.class,
-}
-)
+@SpringBootApplication(scanBasePackages = "com.sssys.grn",
+        exclude = {SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class})
+
 public class GrnServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GrnServerApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  grn-server启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n"
+        );
     }
 }
--- grn-server/src/main/java/com/sssys/grn/controller/demo/HelloController.java
+++ grn-server/src/main/java/com/sssys/grn/controller/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.sssys.grn.controller.demo;
 
+import com.sssys.grn.system.service.impl.SysUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author: captain
  * @Date: 11:01 2025/12/18
  */
-@RestController // 等价于 @Controller + @ResponseBody，直接返回JSON/字符串
-@RequestMapping("/hello") // 统一前缀，区分模块（system模块）
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-//    @Autowired
-//    private SysUserServiceImpl systemUserService;
+    @Autowired
+    private SysUserServiceImpl systemUserService;
     @GetMapping("/world")
     public String helloWorld() {
-        return "Hello, grn-system! 🚀" ;
+        int count = systemUserService.list().size();
+        return "Hello, grn-system! 🚀" +count;
     }
 
 
